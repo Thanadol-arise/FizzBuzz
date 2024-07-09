bae0e8406ef9ff4d30db84b462c04e6d8760d567
Thanadol Thongrit <thanadol.t@arise.tech>
2024-07-09 23:06:26 +0700

    [Normal] Test Case Input 14 Correct

--- Normal/main_test.go
+++ Normal/main_test.go
@@ -131,3 +131,13 @@ func TestInput13(t *testing.T) {
 		t.Errorf("Expected %s but got %s", expected, actual)
 	}
 }
+
+func TestInput14(t *testing.T) {
+	given := 14
+	expected := "14"
+
+	actual := fizzBuzz(given)
+	if actual != expected {
+		t.Errorf("Expected %s but got %s", expected, actual)
+	}
+}
