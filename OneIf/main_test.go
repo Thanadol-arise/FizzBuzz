c52a039c483e228d15c36cf3f1f5a2568fbd6c0e
Thanadol Thongrit <thanadol.t@arise.tech>
2024-07-16 21:42:05 +0700

    [OneIf] Test Case Input 12 Correct

--- OneIf/main_test.go
+++ OneIf/main_test.go
@@ -111,3 +111,13 @@ func TestInput11(t *testing.T) {
 		t.Errorf("Expected %s but got %s", expected, actual)
 	}
 }
+
+func TestInput12(t *testing.T) {
+	given := 12
+	expected := "Fizz"
+
+	actual := fizzBuzz(given)
+	if actual != expected {
+		t.Errorf("Expected %s but got %s", expected, actual)
+	}
+}
