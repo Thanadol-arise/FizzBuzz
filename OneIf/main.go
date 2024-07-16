c50d116452da0a3e06533fb788e5a4da5fe8acd9
Thanadol Thongrit <thanadol.t@arise.tech>
2024-07-16 21:41:35 +0700

    [OneIf] Test Case Input 10 Correct

--- OneIf/main.go
+++ OneIf/main.go
@@ -10,9 +10,10 @@ func fizzBuzz(input int) string {
 		false: "",
 	}[input%3 == 0]
 
-	result += map[int]string{
-		5: "Buzz",
-	}[input]
+	result += map[bool]string{
+		true:  "Buzz",
+		false: "",
+	}[input%5 == 0]
 
 	if result == "" {
 		return strconv.Itoa(input)
