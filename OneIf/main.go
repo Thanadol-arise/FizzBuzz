515149fed124d3a863a3f26fc6751abf6b1cb078
Thanadol Thongrit <thanadol.t@arise.tech>
2024-07-16 12:39:30 +0700

    [OneIf] Test Case Input 3 Correct

--- OneIf/main.go
+++ OneIf/main.go
@@ -1,8 +1,10 @@
 package main
 
+import "strconv"
+
 func fizzBuzz(input int) string {
-	if input == 2 {
-		return "2"
+	if input%3 == 0 {
+		return "Fizz"
 	}
-	return "1"
+	return strconv.Itoa(input)
 }
