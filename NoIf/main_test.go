3e188b43ae8fa4d2de9ed610426363338ea5b0ea
Thanadol Thongrit <thanadol.t@arise.tech>
2024-07-20 17:50:29 +0700

    [NoIf] Test Case Input 15 Correct

--- NoIf/main_test.go
+++ NoIf/main_test.go
@@ -141,3 +141,13 @@ func TestInput14(t *testing.T) {
 		t.Errorf("Expected %s but got %s", expected, actual)
 	}
 }
+
+func TestInput15(t *testing.T) {
+	given := 15
+	expected := "FizzBuzz"
+
+	actual := fizzBuzz(given)
+	if actual != expected {
+		t.Errorf("Expected %s but got %s", expected, actual)
+	}
+}
