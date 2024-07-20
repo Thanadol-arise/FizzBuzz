97380b8ea3b5e42e30256a7d052dfe1f54e27818
Thanadol Thongrit <thanadol.t@arise.tech>
2024-07-21 00:41:20 +0700

    [OOP] Test Case Input 10 Correct

--- OOP/index.js
+++ OOP/index.js
@@ -3,7 +3,7 @@ class Fizzbuzz {
         if (input%3 === 0) {
             return 'Fizz';
         }
-        if (input === 5) {
+        if (input%5 === 0) {
             return 'Buzz';
         }
         return String(input);
