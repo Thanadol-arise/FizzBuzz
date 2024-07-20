ce3d512fdc5c9ed70bf884139f0a80be5149b41d
Thanadol Thongrit <thanadol.t@arise.tech>
2024-07-21 01:45:55 +0700

    [FP] Test Case Input 1 Correct

---
+++ FP/index.test.js
@@ -0,0 +1,9 @@
+const fizzBuzzCalculate  = require('./index.js'); // Import your functions
+
+describe('FizzBuzz', () => {
+    test('input 1', () => {
+        expect(fizzBuzzCalculate(1)).toBe('1');
+    });
+});
+
+
