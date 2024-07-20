34356b7796c89b753901eeb639583fbba8207f09
Thanadol Thongrit <thanadol.t@arise.tech>
2024-07-21 00:37:16 +0700

    [OOP] Test Case Input 2 Correct

--- OOP/index.js
+++ OOP/index.js
@@ -1,5 +1,8 @@
 class Fizzbuzz {
     calculate(input) {
+        if (input === 2) {
+            return '2';
+        }
         return '1';
     }
 }
--- OOP/index.test.js
+++ OOP/index.test.js
@@ -10,5 +10,9 @@ describe('FizzBuzz', () => {
     test('input 1', () => {
         expect(fizzBuzz.calculate(1)).toBe('1');
     });
+
+    test('input 2', () => {
+        expect(fizzBuzz.calculate(2)).toBe('2');
+    });
 });
 
